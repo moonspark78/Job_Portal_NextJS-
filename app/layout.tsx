0932772403c2c8e4a6f54f642ffcec7337175241
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-04-10 21:27:11 +0200

    :art: init   :art:

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Plus_Jakarta_Sans({ 
+  weight: ["200","300","400","500","600","700","800"] 
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
