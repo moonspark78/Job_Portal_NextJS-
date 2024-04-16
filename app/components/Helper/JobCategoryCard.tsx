754d34c4ca8e62ef32d11fb1955518a4f9df18e3
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-04-16 15:41:36 +0200

    :art: job category section  :art:

---
+++ app/components/Helper/JobCategoryCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+interface Props{
+    image: string;
+    category: string;
+    openPosition: string;
+}
+
+const JobCategoryCard = ({image,category,openPosition}:Props) => {
+  return (
+    <div>JobCategoryCard</div>
+  )
+}
+
+export default JobCategoryCard
