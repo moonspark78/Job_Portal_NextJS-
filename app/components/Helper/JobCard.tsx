d2bf82724dc497f40c36d55d1ee69fbbe04a2ae9
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-04-16 20:05:16 +0200

    :art: working on FeaturedJobs  section  :art:

--- app/components/Helper/JobCard.tsx
+++ app/components/Helper/JobCard.tsx
@@ -1,6 +1,7 @@
 import { Job } from '@/data';
 import Image from 'next/image';
 import React from 'react'
+import { BiMoney } from 'react-icons/bi';
 import { FaMapLocation } from 'react-icons/fa6';
 
 interface Props{
@@ -25,11 +26,33 @@ const JobCard = ({job}:Props) => {
                     {job?.title}
                 </h1>
                 <div className='flex items-center md:space-x-10 space-x-4'>
+                    {/* Location */}
                     <div className='flex items-center space-x-2'>
                         <FaMapLocation className='w-[0.8rem] h-[0.8rem] text-pink-700'/>
                         <p className='text-[14px] text-black font-semibold text-opacity-60'>
                             {job?.location}
                         </p>
+                    </div>
+                    {/* Salary */}
+                    <div className='flex items-center space-x-2'>
+                        <BiMoney className='w-[0.8rem] h-[0.8rem] text-pink-700'/>
+                        <p className='text-[14px] text-black font-semibold text-opacity-60'>
+                            {job?.salary}
+                        </p>
+                    </div>
+                </div>
+                <div className='flex items-center space-x-2 sm:space-x-4 mt-[1rem]'>
+                    <div className='text-[10px] sm:text-[13px] text-black text-opacity-80 px-3 sm:px-6 py-1
+                    rounded-full bg-opacity-30 font-semibold capitalize bg-green-400'>
+                        {job?.jobtype}
+                    </div>
+                    <div className='text-[10px] sm:text-[13px] text-black text-opacity-80 px-3 sm:px-6 py-1
+                    rounded-full bg-opacity-30 font-semibold capitalize bg-blue-400'>
+                        Private
+                    </div>
+                    <div className='text-[10px] sm:text-[13px] text-black text-opacity-80 px-3 sm:px-6 py-1
+                    rounded-full bg-opacity-30 font-semibold capitalize bg-red-400'>
+                        Urgent
                     </div>
                 </div>
             </div>
